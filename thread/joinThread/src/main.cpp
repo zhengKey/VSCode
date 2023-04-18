e9542c19227ab24229547d6c255d39edd64dedbe
weimingzheng <weimingzheng@futunn.com>
2023-04-18 10:00:25 +0800

    线程对象析构测试

--- thread/joinThread/src/main.cpp
+++ thread/joinThread/src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <thread>
 using namespace std;
+
+
+void startthread()
+{
+    //这个子线程对象不会析构
+    std::thread th([](){
+        while(1)
+        {
+            std::cout<<"thread  son "<<std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));  // 睡眠 1 秒钟
+        }
+    });
+    th.detach();
+}
+
+
 int main(int argc, char *argv[])
 {
 	std::cout << "Hello world!" << std::endl;
@@ -18,6 +34,7 @@ int main(int argc, char *argv[])
     //说明线程之间的join是不相互阻塞的。当a线程sleep2秒  b可以先执行
 	a.join();
 	ab.join();
-	std::cout<<"main thread";
+    startthread();
+	std::cout<<"main thread"<<std::endl;
 	system("pause");
 }
